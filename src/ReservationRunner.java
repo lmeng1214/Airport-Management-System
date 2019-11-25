8fc9007f1479f376a882580cc24b25d79e011791
Lenny Meng <lmeng1214@gmail.com>
2019-11-25 17:09:30 -0500

    Added runner class to start server. Should be fully functional now except for adding text to reservations.txt

---
+++ src/ReservationRunner.java
@@ -0,0 +1,21 @@
+/**
+ * An initializer for running the server.
+ *
+ * @author Lenny Meng, meng110@purdue.edu
+ * @version Nov 25 2019
+ */
+
+public class ReservationRunner {
+    public static void main(String[] args) {
+        ReservationServer rs;
+
+        try {
+            rs = new ReservationServer();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        rs.start();
+    }
+}
