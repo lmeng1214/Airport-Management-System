650476fd1f7125713517956cabff2ed5be651c7a
jvuskaln <54676486+jvuskaln@users.noreply.github.com>
2019-11-29 20:54:00 -0500

    Was used for testing, program now complete

--- src/Driver.java
+++
@@ -1,6 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        BoardingPass boardingPass = new BoardingPass(new Delta(), new Passenger("19", "Janis", "Vuskalns"), new Gate().generateGate());
-        System.out.print(boardingPass.toString());
-    }
-}
